e2152c4f57f138696ecdbbf9718b7c78eb0326a5
wayne.workman2012@gmail.com <wayne.workman2012@gmail.com>
2017-05-28 15:39:27 -0500

    Fix typo

--- web/verifiedPlayer.php
+++ web/verifiedPlayer.php
@@ -13,7 +13,7 @@ if ($SessionIsVerified == "1") {
     echo "<br><br>\n";
     echo "Public link:<br>\n";
     echo "$domainName/player.php?v=$file.$ext";
-    echo "<br>br>\n";
+    echo "<br><br>\n";
     echo "iframe:<br>\n";
     echo "&lt;iframe width=\"560\" height=\"315\" src=\"$domainName/stream.php?v=$v\" frameborder=\"0\" allowfullscreen&gt;&lt;/iframe&gt";
     echo "<br><br>";
