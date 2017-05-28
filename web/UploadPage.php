b7e11b20a99bbc09f170f1228f44db4f1a754fc5
wayne.workman2012@gmail.com <wayne.workman2012@gmail.com>
2017-05-28 18:59:02 -0500

    Allow authenticated users to upload.

--- web/UploadPage.php
+++ web/UploadPage.php
@@ -2,27 +2,20 @@
 include 'vars.php';
 include 'verifysession.php';
 if ($SessionIsVerified == "1") {
-        if ($isAdministrator == 1) {
-		include 'connect2db.php';
-		include 'head.php';
-		
-		echo "<div>";
-		echo "<br><br>";
-		echo "<form action=\"Upload.php\" method=\"post\" enctype=\"multipart/form-data\">";
-                echo "Video title:<br><input type=\"text\" name=\"vTitle\" id=\"vTitle\"><br><br>";
-		echo "Select file to upload:<br>";
-		echo "<input type=\"file\" name=\"fileToUpload\" id=\"fileToUpload\"><br><br>";
-		echo "<input type=\"submit\" value=\"Upload\" name=\"submit\">";
-		echo "</form>";
-		echo "</div>";
-		echo "</body>";
-		echo "</html>";
 
-        } else {
-                //Not an admin, redirect to home.
-                $NextURL="home.php";
-                header("Location: $NextURL");
-        }
+    include 'connect2db.php';
+    include 'head.php';
+    echo "<div>";
+    echo "<br><br>";
+    echo "<form action=\"Upload.php\" method=\"post\" enctype=\"multipart/form-data\">";
+    echo "Video title:<br><input type=\"text\" name=\"vTitle\" id=\"vTitle\"><br><br>";
+    echo "Select file to upload:<br>";
+    echo "<input type=\"file\" name=\"fileToUpload\" id=\"fileToUpload\"><br><br>";
+    echo "<input type=\"submit\" value=\"Upload\" name=\"submit\">";
+    echo "</form>";
+    echo "</div>";
+    echo "</body>";
+    echo "</html>";
 }
 ?>
 
