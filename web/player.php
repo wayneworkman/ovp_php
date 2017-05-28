b2d94a14a654a00511e45e3f93d2f4c4816fab24
wayne.workman2012@gmail.com <wayne.workman2012@gmail.com>
2017-05-28 15:07:40 -0500

    Fix player a bit.

--- web/player.php
+++ web/player.php
@@ -2,31 +2,31 @@
 
 include 'vars.php';
 include 'verifysession.php';
-if ($SessionIsVerified == "1") {
-    include 'head.php';
-}
-
-
-
 
 $file = $_REQUEST['file'];
 $ext = $_REQUEST['ext'];
 $file = pathinfo($file, PATHINFO_FILENAME);
 $ext = pathinfo($ext, PATHINFO_EXTENSION);
 
-echo "<html>";
-echo "<body>\n";
-echo "<title>\n";
-echo "$filename\n";
-echo "</title>\n";
-echo "<video controls autoplay preload=\"auto\" src=\"stream.php?file=$file&ext=$ext\" width=\"60%\"></video>\n";
-echo "<br><br>\n";
-echo "iframe:<br>\n";
-echo "&lt;iframe width=\"560\" height=\"315\" src=\"http://perpetuum.io/youtube/stream.php?file=$file&ext=$ext\" frameborder=\"0\" allowfullscreen&gt;&lt;/iframe&gt";
-echo "<br><br>";
-echo "html5:<br>\n";
-echo "&lt;video controls autoplay preload=\"auto\" src=\"http://perpetuum.io/youtube/stream.php?file=$file&ext=$ext\" width=\"60%\"&gt;&lt;/video&gt";
-echo "<br><br>";
-echo "</body></html>\n";
+if ($SessionIsVerified == "1") {
+    include 'head.php';
+    echo "<video controls autoplay preload=\"auto\" src=\"stream.php?file=$file&ext=$ext\" width=\"60%\"></video>\n";
+    echo "<br><br>\n";
+    echo "iframe:<br>\n";
+    echo "&lt;iframe width=\"560\" height=\"315\" src=\"http://perpetuum.io/youtube/stream.php?file=$file&ext=$ext\" frameborder=\"0\" allowfullscreen&gt;&lt;/iframe&gt";
+    echo "<br><br>";
+    echo "html5:<br>\n";
+    echo "&lt;video controls autoplay preload=\"auto\" src=\"http://perpetuum.io/youtube/stream.php?file=$file&ext=$ext\" width=\"60%\"&gt;&lt;/video&gt";
+    echo "<br><br>";
+    echo "</body></html>\n";
+} else {
+
+    echo "<html>";
+    echo "<body>\n";
+    echo "<title>\n";
+    echo "$filename\n";
+    echo "</title>\n";
+    echo "<video controls autoplay preload=\"auto\" src=\"stream.php?file=$file&ext=$ext\" width=\"60%\"></video>\n";
+    echo "</body></html>\n";
 ?>
 
