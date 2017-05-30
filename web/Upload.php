1335573bdd84828678ab4726536fc61caccbe134
wayne.workman2012@gmail.com <wayne.workman2012@gmail.com>
2017-05-30 00:16:27 -0500

    Enforce file formats

--- web/Upload.php
+++ web/Upload.php
@@ -62,10 +62,13 @@ if ($SessionIsVerified == "1") {
 
 
 		// Allow certain file formats
-		//if($fileType == "mp4" || $fileType == "webm") {
-		//	$uploadOk = 0;
-		//	$uploadMessage .= "Sorry, only mp4 and webm files are allowed.<br>You uploaded: $fileType";
-		//}
+		if($fileType == "mp4" || $fileType == "webm") {
+			$uploadOk = 0;
+			$uploadMessage .= "Sorry, only mp4 and webm files are allowed.<br>You uploaded: $fileType";
+			$NextURL = "UploadPage.php";
+			header("Location: $NextURL");
+			die;
+		}
 
 
 		// Check if $uploadOk is set to 0 by an error
