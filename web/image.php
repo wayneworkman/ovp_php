ec49b1f826f31ef86dfee688407c5c46ed4ac4eb
wayne.workman2012@gmail.com <wayne.workman2012@gmail.com>
2017-06-10 15:40:53 -0500

    Prevent directory traversal

--- web/image.php
+++ web/image.php
@@ -1,5 +1,6 @@
 <?php
 $remoteImage = $_REQUEST['i'];
+$remoteImage = pathinfo($remoteImage, PATHINFO_FILENAME);
 $remoteImage = "/data/qrCodes/$remoteImage.png";
 $imginfo = getimagesize($remoteImage);
 header("Content-type: {$imginfo['mime']}");
