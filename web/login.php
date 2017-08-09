a7111534497de91dd62e97baaee683653b1a822a
wayne.workman2012@gmail.com <wayne.workman2012@gmail.com>
2017-08-08 21:43:18 -0500

    Shorten login fields

--- web/login.php
+++ web/login.php
@@ -15,8 +15,8 @@ if (isset($_SESSION['badLoginAttempt'])) {
 	}
 }
 
-echo "username <input type=\"text\" name=\"username\">\n";
-echo "<br>password <input type=\"password\" name=\"password\">\n";
+echo "username<br> <input type=\"text\" name=\"username\" style=\"width:600px;\">\n";
+echo "<br>password<br> <input type=\"password\" name=\"password\" style=\"width:600px;\">\n";
 echo "<br>\n";
 echo "<input type=\"submit\" name=\"submit\" value=\"login\">\n";
 echo "</form>\n";
