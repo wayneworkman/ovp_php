90d3459b6e3d21fb818ebead7f47110111ba4626
wayne.workman2012@gmail.com <wayne.workman2012@gmail.com>
2017-08-20 12:25:28 -0500

    Rewrite scripts to not install anything, just copy repo files and enable services.

--- bin/setupConversionNode.sh
+++ bin/setupConversionNode.sh
@@ -1,25 +1,35 @@
 #!/bin/bash
 cwd="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "$cwd/functions.sh"
-source "$cwd/mysqlCredentials.sh"
+
+
+#Get ffmpeg.
+curl --silent https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-64bit-static.tar.xz > /tmp/ffmpeg-release-64bit-static.tar.xz
+#Make directories if not present.
+mkdir -p /data/ffmpeg
+mkdir -p /data/ffmpeg-old
+#Move old versions if present.
+mv /data/ffmpeg/* /data/ffmpeg-old > /dev/null 2>&1
+#Extract.
+tar -xf /tmp/ffmpeg-release-64bit-static.tar.xz -C /data/ffmpeg
+rm -f /tmp/ffmpeg-release-64bit-static.tar.xz
 
 
 
 
-banner
-checkForRoot
-checkOS
-#updateServer
-installCurl
-installMysql
-installQrencode
-getFfmpeg
-checkFfmpeg
-setupConversion
+if [[ -e /data/scripts/mysqlCredentials.sh ]]; then
+    rm -f /data/scripts/mysqlCredentials.sh
+fi
+cp $cwd/mysqlCredentials.sh /data/scripts/mysqlCredentials.sh 
+if [[ -e /usr/lib/systemd/system/processupload.service ]]; then
+    rm -f /usr/lib/systemd/system/processupload.service
+fi
+cp $cwd/processupload.service /usr/lib/systemd/system
+if [[ -e /data/scripts/processupload.sh ]]; then
+    rm -f /data/scripts/processupload.sh
+fi
+cp $cwd/processupload.sh /data/scripts
+systemctl enable processupload.service
+systemctl restart processupload.service
 
 
-
-echo ' '
-echo ' '
-echo 'processupload.service should be running, will immediately begin processing jobs.'
-echo ' '
--- bin/setupWeb.sh
+++ bin/setupWeb.sh
@@ -3,52 +3,29 @@ cwd="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "$cwd/functions.sh"
 source "$cwd/mysqlCredentials.sh"
 
-#Check for passed mysql connection info after sourcing mysql credentials file.
-if [[ ! -z $1 ]]; then
-    mysqlUser="$1"
+
+rm -f /var/www/html/*
+cp ${cwd}/../web/* /var/www/html
+webpermissions="apache:apache"
+chown -R $webpermissions /var/www/html
+
+mkdir -p /data/videos
+mkdir -p /data/deleted
+mkdir -p /data/uploads
+mkdir -p /data/scripts
+mkdir -p /data/logs
+mkdir -p /data/qrCodes
+mkdir -p /data/jobs
+mkdir -p /data/problemJobs
+
+if [[ -e /data/scripts/processupload.sh ]]; then
+    rm -f /data/scripts/processupload.sh
 fi
-if [[ ! -z $2 ]]; then
-    mysqlPass="$2"
-fi
-if [[ ! -z $3 ]]; then
-    mysqlHost="$3"
-fi
-if [[ ! -z $4 ]]; then
-    database="$4"
-fi
+cp $cwd/processupload.sh /data/scripts
+chown -R $webpermissions /data
 
-#Write the vars back to the files because any passed are likely not the ones stored.
-echo "#!/bin/bash" > $cwd/mysqlCredentials.sh
-echo "mysqlUser=\"processvideo\"" >> $cwd/mysqlCredentials.sh
-echo "mysqlPass=\"processvideopassword\"" >> $cwd/mysqlCredentials.sh
-echo "mysqlHost=\"localhost\"" >> $cwd/mysqlCredentials.sh
-echo "database=\"ovp\"" >> $cwd/mysqlCredentials.sh
+systemctl enable httpd
+systemctl start httpd
 
 
 
-post_max_size="5G"
-upload_max_filesize="5G"
-memory_limit="5G"
-max_execution_time="30000"
-max_input_time="30000"
-
-
-
-
-banner
-checkForRoot
-checkOS
-#updateServer
-installWeb "0"
-placeFiles
-configurePHP
-configureApache
-configureFirewalldWeb
-disableSelinux
-
-echo ' '
-echo ' '
-echo 'Default user:pass is:'
-echo 'admin'
-echo 'changeme'
-echo ' '
