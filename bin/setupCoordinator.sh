75ab53d3f51c39b5f1e6bdf600f87e2f9f487871
wayne.workman2012@gmail.com <wayne.workman2012@gmail.com>
2017-08-21 21:15:57 -0500

    Write setup script

--- bin/setupCoordinator.sh
+++ bin/setupCoordinator.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 
+cwd="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+
+if [[ -e /usr/lib/systemd/system/coordinator.service ]]; then
+    rm -f /usr/lib/systemd/system/coordinator.service
+fi
+cp $cwd/coordinator.service /usr/lib/systemd/system
+if [[ -e /data/scripts/coordinator.sh ]]; then
+    rm -f /data/scripts/coordinator.sh
+fi
+cp $cwd/coordinator.sh /data/scripts
+systemctl enable coordinator.service
+systemctl restart coordinator.service
+
 
 
