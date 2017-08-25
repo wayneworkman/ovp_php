aa08a4c2654b733fa35330ce071c5bb77860a934
wayne.workman2012@gmail.com <wayne.workman2012@gmail.com>
2017-08-25 03:48:31 +0000

    include quotes in comparison for health check

--- bin/monitorExactWorker.sh
+++ bin/monitorExactWorker.sh
@@ -37,7 +37,7 @@ checks() {
 
 
     #If this node isn't healthy, kill this node.
-    if [[ $(aws autoscaling describe-auto-scaling-instances --instance-ids $id | jq .AutoScalingInstances[0].HealthStatus) != "HEALTHY" ]]; then
+    if [[ $(aws autoscaling describe-auto-scaling-instances --instance-ids $id | jq .AutoScalingInstances[0].HealthStatus) != "\"HEALTHY\"" ]]; then
         echo "Node $id is not healthy, terminating." >> $log
         $aws autoscaling terminate-instance-in-auto-scaling-group --instance-id $id --should-decrement-desired-capacity
         rm -f ${workers}/${id}
