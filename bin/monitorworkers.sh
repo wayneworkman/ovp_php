49c7d78c6d9b68c1fbdaedc548f94b236cc0a2a5
wayne.workman2012@gmail.com <wayne.workman2012@gmail.com>
2017-08-25 02:29:18 +0000

    Prevent jobs from being abandoned

--- bin/monitorworkers.sh
+++ bin/monitorworkers.sh
@@ -4,10 +4,12 @@
 
 #Variables.
 workers="/data/conversionNodes"
+jobs="/data/jobs"
 
 mkdir -p $workers
 
 while true; do
+
     #This loops through all the worker files and checks for a monitoring process for each found.
     #If one isn't found for it, one gets made and backgrounded.
     for worker in $(find $workers -type f)
@@ -16,7 +18,21 @@ while true; do
         ps -p "$(pidof -x monitorExactWorker.sh)" -o args | grep $worker > /dev/null 2>&1
         [[ $? == 1 ]] && /data/scripts/monitorExactWorker.sh $worker
     done
+
+
+    #If a job has a lock file that is not an actual worker we know of, delete the lock file.
+    for lock in $(find $jobs -type f -name '*.lock')
+    do
+        lockID=$(cat $lock | head -n 1)
+        if [[ ! -e ${workers}/${lockID} ]]; then
+            rm -f $lock
+        fi
+    done
+
+
+    #How often to do this stuff.
     sleep 7
+
 done
 
 
